e05ae54646ad57143bfb85b5a21df716e24bbaaa
Sharky <osmiov.yura@gmail.com>
2023-04-19 06:11:30 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace SotItOut2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.DoneButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.UpButton = new System.Windows.Forms.Button();
+            this.DownButton = new System.Windows.Forms.Button();
+            this.TopButton = new System.Windows.Forms.Button();
+            this.BottomButton = new System.Windows.Forms.Button();
+            this.SelectAll = new System.Windows.Forms.Button();
+            this.UnselectAll = new System.Windows.Forms.Button();
+            this.Sort = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // DoneButton
+            // 
+            this.DoneButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DoneButton.Location = new System.Drawing.Point(12, 487);
+            this.DoneButton.Name = "DoneButton";
+            this.DoneButton.Size = new System.Drawing.Size(130, 43);
+            this.DoneButton.TabIndex = 0;
+            this.DoneButton.Text = "Complete";
+            this.DoneButton.UseVisualStyleBackColor = true;
+            this.DoneButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitButton.Location = new System.Drawing.Point(148, 487);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(130, 43);
+            this.ExitButton.TabIndex = 1;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.ScrollAlwaysVisible = true;
+            this.checkedListBox1.Size = new System.Drawing.Size(266, 469);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // UpButton
+            // 
+            this.UpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpButton.Location = new System.Drawing.Point(284, 41);
+            this.UpButton.Name = "UpButton";
+            this.UpButton.Size = new System.Drawing.Size(75, 23);
+            this.UpButton.TabIndex = 3;
+            this.UpButton.Text = "🔼";
+            this.UpButton.UseVisualStyleBackColor = true;
+            this.UpButton.Click += new System.EventHandler(this.upButton_Click);
+            // 
+            // DownButton
+            // 
+            this.DownButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DownButton.Location = new System.Drawing.Point(284, 70);
+            this.DownButton.Name = "DownButton";
+            this.DownButton.Size = new System.Drawing.Size(75, 23);
+            this.DownButton.TabIndex = 4;
+            this.DownButton.Text = "🔽";
+            this.DownButton.UseVisualStyleBackColor = true;
+            this.DownButton.Click += new System.EventHandler(this.downButton_Click);
+            // 
+            // TopButton
+            // 
+            this.TopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TopButton.Location = new System.Drawing.Point(284, 12);
+            this.TopButton.Name = "TopButton";
+            this.TopButton.Size = new System.Drawing.Size(75, 23);
+            this.TopButton.TabIndex = 5;
+            this.TopButton.Text = "⏫";
+            this.TopButton.UseVisualStyleBackColor = true;
+            this.TopButton.Click += new System.EventHandler(this.topButton_Click);
+            // 
+            // BottomButton
+            // 
+            this.BottomButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BottomButton.Location = new System.Drawing.Point(284, 99);
+            this.BottomButton.Name = "BottomButton";
+            this.BottomButton.Size = new System.Drawing.Size(75, 23);
+            this.BottomButton.TabIndex = 6;
+            this.BottomButton.Text = "⏬";
+            this.BottomButton.UseVisualStyleBackColor = true;
+            this.BottomButton.Click += new System.EventHandler(this.bottomButton_Click);
+            // 
+            // SelectAll
+            // 
+            this.SelectAll.Location = new System.Drawing.Point(284, 458);
+            this.SelectAll.Name = "SelectAll";
+            this.SelectAll.Size = new System.Drawing.Size(75, 23);
+            this.SelectAll.TabIndex = 7;
+            this.SelectAll.Text = "Select All";
+            this.SelectAll.UseVisualStyleBackColor = true;
+            this.SelectAll.Click += new System.EventHandler(this.selectAll_Click);
+            // 
+            // UnselectAll
+            // 
+            this.UnselectAll.Location = new System.Drawing.Point(284, 429);
+            this.UnselectAll.Name = "UnselectAll";
+            this.UnselectAll.Size = new System.Drawing.Size(75, 23);
+            this.UnselectAll.TabIndex = 8;
+            this.UnselectAll.Text = "Unselect All";
+            this.UnselectAll.UseVisualStyleBackColor = true;
+            this.UnselectAll.Click += new System.EventHandler(this.unselectAll_Click);
+            // 
+            // Sort
+            // 
+            this.Sort.Location = new System.Drawing.Point(284, 487);
+            this.Sort.Name = "Sort";
+            this.Sort.Size = new System.Drawing.Size(75, 43);
+            this.Sort.TabIndex = 9;
+            this.Sort.Text = "Sort";
+            this.Sort.UseVisualStyleBackColor = true;
+            this.Sort.Click += new System.EventHandler(this.Sort_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InfoText;
+            this.ClientSize = new System.Drawing.Size(365, 538);
+            this.Controls.Add(this.Sort);
+            this.Controls.Add(this.UnselectAll);
+            this.Controls.Add(this.SelectAll);
+            this.Controls.Add(this.BottomButton);
+            this.Controls.Add(this.TopButton);
+            this.Controls.Add(this.DownButton);
+            this.Controls.Add(this.UpButton);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.DoneButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Sort It Out 2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DoneButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button UpButton;
+        private System.Windows.Forms.Button DownButton;
+        private System.Windows.Forms.Button TopButton;
+        private System.Windows.Forms.Button BottomButton;
+        private System.Windows.Forms.Button SelectAll;
+        private System.Windows.Forms.Button UnselectAll;
+        private System.Windows.Forms.Button Sort;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SotItOut2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            string dir = Directory.GetCurrentDirectory();
+            DirectoryInfo di = new DirectoryInfo(dir);
+            DirectoryInfo[] dirs = di.GetDirectories();
+
+
+            InitializeComponent();
+
+
+
+            foreach (var folder in dirs)
+            {
+                if (folder.Name != "base" && folder.Name != "dmf")
+                {
+                    checkedListBox1.Items.Add(folder);
+                }
+            }
+
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string name = new DirectoryInfo(dir).Name;
+
+            if (name != "mods")
+            {
+                MessageBox.Show("This program should be located in 'mods' folder!", "Error!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+
+            toolTip1.SetToolTip(Sort, "Sorts current list with compatibility in mind");
+            toolTip1.SetToolTip(DoneButton, "Creates mod list file using checked items in current list");
+            toolTip1.SetToolTip(ExitButton, "Closes the program");
+            toolTip1.SetToolTip(SelectAll, "Selects everything in current list");
+            toolTip1.SetToolTip(UnselectAll, "Unselects everything in current list");
+            toolTip1.SetToolTip(TopButton, "Moves selected item on the top of the list");
+            toolTip1.SetToolTip(UpButton, "Moves selected item one line higher in the list");
+            toolTip1.SetToolTip(DownButton, "Moves selected item one line lower in the list");
+            toolTip1.SetToolTip(BottomButton, "Moves selected item on the bottom of the list");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string file = (dir + @"\mod_load_order.txt");
+
+            using (StreamWriter ml = new StreamWriter(file))
+            {
+                foreach (var item in checkedListBox1.CheckedItems)
+                {
+                    ml.WriteLine(item);
+                }
+            } 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void upButton_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.SelectedItems.Count > 0)
+            {
+                object item = checkedListBox1.SelectedItem;
+                int index = checkedListBox1.SelectedIndex - 1;
+                bool isChecked = checkedListBox1.GetItemCheckState(index + 1) == CheckState.Checked;
+                if (index >= 0)
+                {
+                    checkedListBox1.Items.Remove(item);
+                    checkedListBox1.Items.Insert(index, item);
+                    index = checkedListBox1.Items.IndexOf(item);
+                    checkedListBox1.SetSelected(index, true);
+                    if (isChecked)
+                    {
+                        checkedListBox1.SetItemChecked(index, true);
+                    }
+                }
+            }
+        }
+
+        private void downButton_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.SelectedItems.Count > 0)
+            {
+                object item = checkedListBox1.SelectedItem;
+                int index = checkedListBox1.SelectedIndex + 1;
+                bool isChecked = checkedListBox1.GetItemCheckState(index - 1) == CheckState.Checked;
+                if (index < checkedListBox1.Items.Count)
+                {
+                    checkedListBox1.Items.Remove(item);
+                    checkedListBox1.Items.Insert(index, item);
+                    index = checkedListBox1.Items.IndexOf(item);
+                    checkedListBox1.SetSelected(index, true);
+                    if (isChecked)
+                    {
+                        checkedListBox1.SetItemChecked(index, true);
+                    }
+                }
+            }
+        }
+
+        private void topButton_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.SelectedItems.Count > 0)
+            {
+                object item = checkedListBox1.SelectedItem;
+                int index = checkedListBox1.Items.IndexOf(item);
+                bool isChecked = checkedListBox1.GetItemCheckState(index) == CheckState.Checked;
+                if (item != null)
+                {
+                    checkedListBox1.Items.Remove(item);
+                    checkedListBox1.Items.Insert(0, item);
+                    index = checkedListBox1.Items.IndexOf(item);
+                    checkedListBox1.SetSelected(index, true);
+                    if (isChecked)
+                    {
+                        checkedListBox1.SetItemChecked(index, true);
+                    }
+                }
+            }
+        }
+
+        private void bottomButton_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.SelectedItems.Count > 0)
+            {
+                object item = checkedListBox1.SelectedItem;
+                int index = checkedListBox1.Items.IndexOf(item);
+                bool isChecked = checkedListBox1.GetItemCheckState(index) == CheckState.Checked;
+                if (item != null)
+                {
+                    checkedListBox1.Items.Remove(item);
+                    checkedListBox1.Items.Add(item);
+                    index = checkedListBox1.Items.IndexOf(item);
+                    checkedListBox1.SetSelected(index, true);
+                    if (isChecked)
+                    {
+                        checkedListBox1.SetItemChecked(index, true);
+                    }
+                }
+            }
+        }
+
+        private void selectAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+        }
+
+        private void unselectAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, false);
+            }
+        }
+
+        private void Sort_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+
+            string[] topList = { "LogMeIn" };
+            string[] bottomList = { "animation_events",
+                                    "ui_extension",
+            };
+
+            foreach (var item in checkedListBox1.Items)
+            {
+                list.Add(item.ToString());
+            }
+
+            checkedListBox1.Items.Clear();
+            list.Sort();
+
+            foreach (var item in list)
+            {
+                checkedListBox1.Items.Add(item.ToString());
+            }
+
+            for (int i = 0; i < topList.Length; i++)
+            {
+                if (checkedListBox1.Items.Contains(topList[i]))
+                {
+                    checkedListBox1.Items.Remove(topList[i]);
+                    checkedListBox1.Items.Insert(0, topList[i]);
+                }
+            }
+
+            for (int i = 0; i < bottomList.Length; i++)
+            {
+                if (checkedListBox1.Items.Contains(bottomList[i]))
+                {
+                    checkedListBox1.Items.Remove(bottomList[i]);
+                    checkedListBox1.Items.Add(bottomList[i]);
+                }
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+        }
+    }
+}
